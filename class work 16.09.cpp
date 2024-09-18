af20dba3279f6393657142de9635c90f49cd935b
MishaPetrovskui <154260100+MishaPetrovskui@users.noreply.github.com>
2024-09-18 18:22:00 +0300

    Update class work 16.09.cpp

--- class work 16.09.cpp
+++ class work 16.09.cpp
@@ -10,14 +10,81 @@ int randint(int min, int max)
 
 int main()
 {
+
 	srand(time(nullptr));
 	const int len = 10;
 	int array[len];
-
+	int _;
 	for (int i = 0; i < len; i++)
 	{
-		array[i] = randint(-20, 20);
+		array[i] = randint(1, 12);
 	}
+	while (true)
+	{
+		int a;
+		cout << "1.Start\n2.Exit" << endl;
+		cin >> a;
+		if (a == 1) 
+		{
+			while (true)
+			{
+				cout << "1.Vuvid ochinok\n2.Peresdacha ispitu\n3.Chi vihodit stependiya" << endl;
+				cin >> _;
+				if (_ == 1)
+				{
+					for (int i = 0; i < len; i++)
+					{
+						cout << i + 1 << ". " << array[i] << "  ";
+					}
+					cout << "\n";
+				}
+				else if (_ == 2)
+				{
+					int number;
+					int new1;
+					for (int i = 0; i < len; i++)
+					{
+						cout << i + 1 << array[i] << "  ";
+					}
+					cout << "Nomer elementa,kotoriy hojeh izmenit: ";
+					cin >> number;
+					cout << "Novaya ohenka: ";
+					cin >> new1;
+					array[number - 1] = new1;
+					break;
+				}
+				else if (_ == 3)
+				{
+					int suma = 0;
+					for (int i = 0; i < len; i++)
+					{
+						suma += array[i];
+					}
+					if (suma / len >= 10)
+					{
+						cout << "Vishla stependiya" << endl;
+						break;
+					}
+					else
+					{
+						cout << "Ne vishla stependiya" << endl;
+						break;
+					}
+				}
+			}
+		}
+		else if (a == 2)
+		{
+			break;
+		}
+		else
+		{
+			cout << "Nepravilno" << endl;
+		}
+	}
+	
+	/*
+	
 	for (int i = 0; i < len; i++)
 	{
 		cout << array[i] << "  ";
@@ -29,7 +96,7 @@ int main()
 			cout << array[i] << "  ";
 		}
 	}
-	
+	*/
 
 	return 1;
 }
