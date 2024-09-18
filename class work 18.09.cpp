79d89ca83c6ee839e8279afd5ef6e9e24cfff281
MishaPetrovskui <154260100+MishaPetrovskui@users.noreply.github.com>
2024-09-18 19:54:06 +0300

    Create class work 18.09.cpp

---
+++ class work 18.09.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+using namespace std;
+
+int randint(int min, int max)
+{
+	return (rand() % (max - min + 1)) + min;
+}
+
+
+int main() {
+
+	srand(time(nullptr));
+	const int rows = 6, columns = 6;
+	int matrix[rows][columns];
+	double sum = 0;
+	int max = matrix[rows][columns];
+	int min = matrix[rows][columns];
+
+	for (int i = 0; i < rows; i++)
+	{
+		for (int j = 0; j < columns; j++)
+		{
+			matrix[i][j] = randint(-20, 20);
+		}
+	}
+
+	for (int i = 0; i < rows; i++)
+	{
+		for (int j = 0; j < columns; j++)
+		{
+			std::cout << matrix[i][j] << "\t";
+			sum += matrix[i][j];
+			if (matrix[i][j] > max)
+			{
+				max = matrix[i][j];
+			}
+			if (matrix[i][j] < min)
+			{
+				min = matrix[i][j];
+			}
+		}
+		cout << " | "; cout << "Suma: " << sum; cout << ", ";
+		cout << "Seredne arifmetichne: " << double(sum / (rows * columns));
+		cout << ", "; cout << "max = " << max; cout << ", "; cout << "min = " << min << endl;
+		cout << endl;
+	}
+	cout << endl << endl << endl;
+	cout << "Suma vsih elementiv: " << sum << endl;
+	cout << "Seredne arifmetichne vsih elementiv: " << double(sum / (rows * columns)) << endl;
+	cout << "max = " << max << endl;
+	cout << "min = " << min;
+
+
+
+	return 0;
+}
