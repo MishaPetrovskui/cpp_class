c92d1ad96c378866a300253179549f2b5712013a
MishaPetrovskui <154260100+MishaPetrovskui@users.noreply.github.com>
2024-09-30 19:20:05 +0300

    Update class work 30.09.24.cpp

--- class work 30.09.24.cpp
+++ class work 30.09.24.cpp
@@ -8,6 +8,13 @@ int randint(int min, int max)
 	return (rand() % (max - min + 1)) + min;
 }
 
+void SetColor(int textColor, int bgColor)
+{
+	HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+	SetConsoleTextAttribute(hConsole,
+		(bgColor << 4) | textColor);
+}
+
 void print_array(int* array, int size)
 {
 	for (int i = 0; i < size; i++)
@@ -16,21 +23,52 @@ void print_array(int* array, int size)
 	}
 	cout << "\b\b." << endl;
 }
+void print_array(int* array[], int size)
+{
+	for (int i = 0; i < size; i++)
+	{
+		cout << array[i] << ", ";
+	}
+	cout << "\b\b." << endl;
+}
+
+float suma(int* array, int size)
+{
+	float sumir = 0;
+	for (int i = 0; i < size; i++)
+	{
+		sumir += array[i];
+	}
+	return sumir;
+}
+
+float seredne_arifmetichne(int* array, int size)
+{
+	float i = suma(array, size) / size;
+	return i;
+}
 
 int main()
 {
 	srand(time(NULL));
-	int size = 0;
+	int size;
 	cout << "int size: ";
 	cin >> size;
-	int* array = new int[size];
-
-	for (int i = 0; i < size; i++)
+	if (0 < size < 100)
 	{
-		array[i] = randint(-50,50);
+		int* array = new int[size];
+
+		for (int i = 0; i < size; i++)
+		{
+			array[i] = randint(0, 1);
+		}
+		SetColor(4, 0);
+		print_array(array, size);
+		cout << "Suma: " << suma(array, size) << "; Seredne arifmetichne: " << seredne_arifmetichne(array, size);
+		delete[] array;
 	}
-	print_array(array, size);
-	delete [] array;
+	else
+		cout << "Ne v diapozone";
 
 	return 0;
 }
