4f851183a60d2936844f834897af46a0fd7c7c40
MishaPetrovskui <154260100+MishaPetrovskui@users.noreply.github.com>
2024-09-30 20:46:54 +0300

    Update class work 30.09.24.cpp

--- class work 30.09.24.cpp
+++ class work 30.09.24.cpp
@@ -48,27 +48,64 @@ float seredne_arifmetichne(int* array, int size)
 	return i;
 }
 
+bool find_in_array(int value, int array[], int size)
+{
+	for (int i = 0; i < value; i++)
+		if (array[i] == value)
+			return true;
+	return false;
+}
+
 int main()
 {
 	srand(time(NULL));
-	int size;
-	cout << "int size: ";
-	cin >> size;
-	if (0 < size < 100)
+	int size1 = 0;
+	int size2 = 0;
+	int size3 = 0;
+	cout << "int size1: ";
+	cin >> size1;
+	cout << "int size2: ";
+	cin >> size2;
+	size3 = size1 + size2;
+	int* array1 = new int[size1];
+	int* array2 = new int[size2];
+	int* array3 = new int[size3];
+	
+	for (int i = 0; i < size1; i++)
 	{
-		int* array = new int[size];
-
-		for (int i = 0; i < size; i++)
-		{
-			array[i] = randint(0, 1);
-		}
-		SetColor(4, 0);
-		print_array(array, size);
-		cout << "Suma: " << suma(array, size) << "; Seredne arifmetichne: " << seredne_arifmetichne(array, size);
-		delete[] array;
+		array1[i] = randint(0, 100);
 	}
-	else
-		cout << "Ne v diapozone";
+	print_array(array1, size1);
+	for (int i = 0; i < size2; i++)
+	{
+		array2[i] = randint(0, 100);
+	}
+	print_array(array2, size2);
+	for (int i = 0; i < size1; i++)
+	{
+		array3[i] = array1[i];
+	}
+	print_array(array3, size3);
+	for (int i = size1; i < size1 + size2; i++)
+	{
+		array3[i] = array2[i-size1];
+	}
+
+	SetColor(2, 0);
+	print_array(array1, size1);
+	print_array(array2, size2);
+	print_array(array3, size3);
+
+	int unique_values = 0;
+	for (int i = 0; i < size3; i++)
+		if (!find_in_array(array3[i], array3, size3))
+			unique_values++;
+
+	int* unique_array_size = new int[unique_values];
+	
+
+	delete[] array1;
+	delete[] array2;
 
 	return 0;
 }
