7b6ba78ac5c5cb26819d893d2be7c33540a420e7
MishaPetrovskui <154260100+MishaPetrovskui@users.noreply.github.com>
2024-11-18 19:21:49 +0200

    Update classwork 18.11.24.cpp

--- classwork 18.11.24.cpp
+++ classwork 18.11.24.cpp
@@ -29,6 +29,7 @@ int main() {
     
     file.close();
     */
+    /*
     file.open(FILE_PATH, ios::in);
     while (file)
     {
@@ -36,6 +37,64 @@ int main() {
         //file >> text;
         getline(file, text);
         cout << text << endl;
+    }
+    */
+    while (true)
+    {
+        cout << "MENU:\n1)Add text\n2)Out text\n3)New file\n4)Max len\n0)Exit\nAction: ";
+        int nAction;
+        cin >> nAction;
+        system("cls");
+        if (nAction == 0)
+        {
+            break;
+        }
+        else if (nAction == 1)
+        {
+            fstream file(FILE_PATH, ios::app);
+            cout << "Input text: ";
+            string a;
+            cin >> a;
+            file << a << endl;
+            file.close();
+        }
+        else if (nAction == 2)
+        {
+            file.open(FILE_PATH, ios::in);
+            int _ = 1;
+            while (file)
+            {
+                string text;
+                //file >> text;
+                getline(file, text);
+                cout << _++ <<". " << text << endl;
+            }
+            file.close();
+        }
+        else if (nAction == 3)
+        {
+            file.open(FILE_PATH, ios::out);
+            file.close();
+        }
+        else if (nAction == 4)
+        {
+            file.open(FILE_PATH, ios::in);
+            int file_size = 0;
+            string text, corect_text;
+            while (file)
+            {
+                getline(file, text);
+                if (text.length() > file_size)
+                {
+                    file_size = text.length();
+                    corect_text = text;
+                }
+            }
+            cout << corect_text << endl << file_size << " size" << endl;
+            file.close();
+        }
+        else
+            cout << "ERROR!";
     }
 
     return 0;
